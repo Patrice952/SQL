70a37e7b002207d8f5276f252b6d5f85d4f3e6a3
Patrice952 <148568310+Patrice952@users.noreply.github.com>
2023-12-14 23:13:41 +0100

    requête SQL partie 2 .sql

--- requête sql partie 2 .sql
+++ requête sql partie 2 .sql
@@ -1,16 +1,24 @@
+--: Le salaire de base d’un professeur doit être inférieur au salaire actuel--
 ALTER TABLE PROFESSEURS
 
 ADD   CONSTRAINT CHK_SALAIRE CHECK (SALAIRE_BASE <= SALAIRE_ACTUEL);
 
 
+
+--Le sexe d'un étudiant doit être dans la liste: 'm', 'M', 'f', 'F' ou Null--
 ALTER TABLE [ELEVES] 
  
   ADD CHECK  (([SEXE]='F' OR [SEXE]='f' OR [SEXE]='M' OR [SEXE]='m' OR [SEXE] IS NULL));
 
+
+--La note d'un étudiant doit être comprise entre 0 et 20--
 ALTER TABLE RESULTATS
-ADD CONSTRAINT NOTE CHECK ([POINTS]>=(0) AND [POINTS]<=(20));
+
+ ADD CONSTRAINT NOTE CHECK ([POINTS]>=(0) AND [POINTS]<=(20));
 
 
+
+--trigger pour la non diminution de salaire--
 CREATE TRIGGER tr_SALAIRE_NOT_DIMINUED
 ON PROFESSEURS
 AFTER UPDATE
@@ -31,7 +39,7 @@ BEGIN
 END;
 
 
-
+--creation de la table pour prochain exo--
 CREATE TABLE PROF_SPECIALITE (
     SPECIALITE VARCHAR(20), 
     NB_PROFESSEURS INT);
@@ -39,7 +47,7 @@ CREATE TABLE PROF_SPECIALITE (
 
 
 
-
+--trigger pour maj INSERT, UPDATE, DELETE de la table professeurs--
 CREATE TRIGGER TR_PROFESSEURS_MAJ
 ON PROFESSEURS
 AFTER INSERT, UPDATE, DELETE
@@ -63,14 +71,16 @@ BEGIN
     WHERE UP.SPECIALITE IS NULL;
 END;
 
+--vérification de la table--
 SELECT * FROM PROFESSEURS
 
+ --test du trigger--
 UPDATE PROFESSEURS
-SET SPECIALITE = 'Autre Spécialité'
+SET SPECIALITE = 'Boxe'
 WHERE NUM_PROF = 1;
 
 
-
+--trigger  table charge pour supp prof ou changer son num--
 CREATE TRIGGER tr_Charge
 ON PROFESSEURS
 AFTER DELETE, UPDATE
@@ -96,12 +106,12 @@ BEGIN
     END;
 END;
 
-
+--test du trigger faire un SELECT aussi--
 DELETE FROM PROFESSEURS
 WHERE NUM_PROF = 5;
 
 
-
+--fonction pour calculer la moyenne d'un étudiant
 CREATE FUNCTION fn_moyenne (@num_eleve INT)
 RETURNS INT
 AS
@@ -116,7 +126,7 @@ BEGIN
 END;
 
 
-
+--test de la fonction
 DECLARE @moyennePoints INT;
 
 EXEC @moyennePoints = fn_moyenne @num_eleve = 1; 
@@ -124,7 +134,7 @@ EXEC @moyennePoints = fn_moyenne @num_eleve = 1;
 PRINT 'Moyenne des points : ' + CAST(@moyennePoints AS VARCHAR(20));
 
 
-
+--procédure pour crer une mention adéquate--
 CREATE PROCEDURE pr_resultat
 AS
 BEGIN
@@ -145,5 +155,5 @@ BEGIN
         RESULTATS R ON E.NUM_ELEVE = R.NUM_ELEVE;
 END;
 
-
+--test de la procédure--
 EXEC pr_resultat
